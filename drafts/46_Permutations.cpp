4f27c630971a282975d7570a35c7b5673c440354
Hong <hongzhihua@gmail.com>
2015-02-02 23:12:02 +0800

    wip

---
+++ drafts/46_Permutations.cpp
@@ -0,0 +1,114 @@
+/*
+ * Given a collection of numbers, return all possible permutations.
+ * 
+ * For example,
+ * [1,2,3] have the following permutations:
+ * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ */
+
+// Tag:
+
+//#include "gtest/gtest.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int>> solutions;
+        vector<int> candidate;
+
+        if (num.empty()) {
+            return solutions;
+        }
+
+        permute_bt(num, solutions, candidate);
+
+        print(solutions[0]);
+        return solutions;
+    }
+
+    // backtracking
+    // permutation is like 8-queen problem with less constraint, just being in
+    // different rows and columns
+    void permute_bt(const vector<int> &num, vector<vector<int>> &solutions, vector<int> &candidate) {
+        static auto size = num.size();
+
+        if (reject(candidate)) {
+            //cout <<"reject ";
+            //print(candidate);
+            return;
+        }
+        if (pass(candidate, size)) {
+            //cout <<"pass" ;
+            //print(candidate);
+            solutions.push_back(candidate);
+            print(solutions.back());
+            return;
+        }
+        if (candidate.size() == size) {
+            return;
+        }
+
+        for (int i = 0; i < size; ++i) {
+            candidate.push_back(num[i]);
+            permute_bt(num, solutions, candidate);
+            candidate.pop_back();
+        }
+
+    }
+
+    bool reject(const vector<int> &candidate) {
+        auto size = candidate.size();
+        if (size == 0) {
+            return false;
+        }
+        
+        int last = candidate.back();
+        for (int i = 0; i < size - 1; ++i) {
+            if (candidate[i] == last) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool pass(const vector<int> &candidate, int size) {
+        if (candidate.size() != size) {
+            return false;
+        }
+
+        int last = candidate.back();
+        for (int i = 0; i < size - 1; ++i) {
+            if (candidate[i] == last) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // tmp
+    void print(const vector<int> &v) {
+        auto iter = v.begin();
+        auto end = v.end();
+        while (iter != end) {
+            cout <<*(iter++) << " ";
+        }
+        cout <<endl;
+    }
+
+    // todo: no bt version
+};
+
+// tests begin
+//TEST(Problem46Test, Test1) {
+int main(int argc, char **argv) {
+    Solution s;
+    vector<int> num;
+    num.push_back(0);
+    num.push_back(1);
+
+    s.permute(num);
+}
